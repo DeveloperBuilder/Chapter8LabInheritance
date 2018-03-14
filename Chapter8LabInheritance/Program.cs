005e3e6e7458ff9727d43677ed2a1382bed4be3c
LSLIM <LSLIM@LS>
2018-03-14 20:35:46 +0100

    Add project files.

---
+++ Chapter8LabInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8LabInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
